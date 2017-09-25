4b8f2e054f0f774baadcba581ea6928363e0a3b3
zhangruixuan <1701210422@pku.edu.cn>
2017-09-25 17:14:07 +0800

    first commit

---
+++ app/src/main/java/com/example/kk/miniweather/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.kk.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kk on 2017/9/20.
+ */
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
